6889208a573585e354583d0bb2edf007d7431bfb
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-07-21 16:17:04 +0200

    Ajout d'un editeur markdown

---
+++ src/components/note/InitializedMDXEditor.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import type { ForwardedRef } from 'react';
+import {
+    headingsPlugin,
+    listsPlugin,
+    quotePlugin,
+    thematicBreakPlugin,
+    markdownShortcutPlugin,
+    linkPlugin,
+    linkDialogPlugin,
+    MDXEditor,
+    toolbarPlugin,
+    Separator,
+    UndoRedo,
+    BoldItalicUnderlineToggles,
+    ListsToggle,
+    BlockTypeSelect,
+    CreateLink,
+    InsertThematicBreak,
+    type MDXEditorMethods,
+    type MDXEditorProps,
+} from '@mdxeditor/editor';
+
+import '@mdxeditor/editor/style.css';
+import './style.css';
+import { MDXEditorPropsAndToolbar } from './mdxEditor';
+
+// Only import this to the next file
+export default function InitializedMDXEditor({
+    editorRef,
+    ...props
+}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorPropsAndToolbar) {
+    return (
+        <MDXEditor
+            className="je-theme"
+            plugins={[
+                ((props.displayToolbar ?? true) &&
+                    toolbarPlugin({
+                        toolbarContents: () => (
+                            <>
+                                <UndoRedo />
+                                <Separator />
+                                <BoldItalicUnderlineToggles />
+                                <Separator />
+                                <ListsToggle />
+                                <Separator />
+                                <BlockTypeSelect />
+                                <Separator />
+                                <CreateLink />
+                                <Separator />
+                                <InsertThematicBreak />
+                            </>
+                        ),
+                    })) as any, // Bricolage par ce que j'ai la flemme de faire un push dans un array
+                headingsPlugin(),
+                quotePlugin(),
+                listsPlugin(),
+                thematicBreakPlugin(),
+                markdownShortcutPlugin(),
+                linkPlugin(),
+                linkDialogPlugin(),
+            ]}
+            {...props}
+            ref={editorRef}
+        />
+    );
+}
