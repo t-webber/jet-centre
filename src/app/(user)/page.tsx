2a7a2936a4775e24e9c44d1cc3fcdc1d9d18f105
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-14 01:36:09 +0100

    Add button the create new study

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,11 +1,20 @@
 import { Metadata } from 'next';
 
+import { Button } from '@/components/ui/button';
+
 export const metadata = {
-    title: 'Dashboard',
+    title: 'Dashboard'
 } satisfies Metadata;
 
 const HomePage = () => {
-    return <div>Bienvenue sur le site de Telecom Etude</div>;
+    return (
+        <>
+            <div className="mb-4">Bienvenue sur le site de Telecom Etude</div>
+            <Button>
+                <a href="/create">Nouvelle étude</a>
+            </Button>
+        </>
+    );
 };
 
 export default HomePage;
