dae7de614d354eaa6b66753b805289cb12262c5d
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-14 01:21:06 +0100

    (mri): Complete form with all fields required to write an MRI
    - Add Difficulty fields
    - Reorder fields based on the order they appear on the mail

--- src/app/(user)/(cdp)/[etude]/mri/form/form.tsx
+++ src/app/(user)/(cdp)/[etude]/mri/form/form.tsx
@@ -1,14 +1,8 @@
-'use client';
-
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-
-import { Form } from '@/components/ui/form';
-import { DOMAINS, FormType, PAYS, defaultMriCreationSchema, mriCreationSchema } from './schema';
+import { Form, FormRow } from '@/components/ui/form';
+import { DIFFICULTIES, DOMAINS, PAYS } from './schema';
 import { InputFormElement } from '@/components/meta-components/form/input';
 import { DropdownFormElement } from '@/components/meta-components/form/dropdown';
 import { TextAreaFormElement } from '@/components/meta-components/form/textarea';
-import { useEffect } from 'react';
 
 export default function MRICreationForm({ form }: { form: any }) {
     return (
@@ -16,28 +10,45 @@ export default function MRICreationForm({ form }: { form: any }) {
             <form className="space-y-5 py-5">
                 <InputFormElement label="Titre" name="title" form={form} />
                 <TextAreaFormElement label="Introduction" name="intro" form={form} resizable />
+
+                <hr />
+
+                <DropdownFormElement label="Domaine" name="domain" values={DOMAINS} form={form} />
+                <FormRow>
+                    <InputFormElement
+                        className="w-1/3"
+                        label="Rétribution basse"
+                        name="pay_under"
+                        type="number"
+                        form={form}
+                    />
+                    <InputFormElement
+                        className="w-1/3"
+                        label="Rétribution haut"
+                        name="pay_over"
+                        type="number"
+                        form={form}
+                    />
+                    <DropdownFormElement
+                        className="w-1/3"
+                        label="Rétribution"
+                        name="pay_level"
+                        values={PAYS}
+                        form={form}
+                    />
+                </FormRow>
+                <DropdownFormElement
+                    label="Difficulté"
+                    name="difficulty"
+                    values={DIFFICULTIES}
+                    form={form}
+                />
+
+                <hr />
+
                 <TextAreaFormElement label="Compétences" name="skills" form={form} resizable />
                 <TextAreaFormElement label="Échéances" name="due_date" form={form} resizable />
                 <TextAreaFormElement label="Description" name="description" form={form} resizable />
-                <DropdownFormElement label="Domaine" name="domain" values={DOMAINS} form={form} />
-                <DropdownFormElement
-                    label="Rétribution"
-                    name="pay_level"
-                    values={PAYS}
-                    form={form}
-                />
-                <InputFormElement
-                    label="Rétribution basse"
-                    name="pay_under"
-                    type="number"
-                    form={form}
-                />
-                <InputFormElement
-                    label="Rétribution haut"
-                    name="pay_over"
-                    type="number"
-                    form={form}
-                />
             </form>
         </Form>
     );
