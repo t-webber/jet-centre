98ee817a8441d9caca68c5b34173e90e785e42ac
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-14 19:23:09 +0100

    update frontend according to figma model

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -22,7 +22,10 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         return (
             <textarea
                 className={cn(
-                    'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                    'flex min-h-[80px] w-full rounded-md px-3 py-2 text-sm border border-input bg-box-background ring-offset-background overflow-y-hidden',
+                    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+                    'disabled:cursor-not-allowed disabled:opacity-50',
+                    'placeholder:text-muted-foreground',
                     className
                 )}
                 ref={ref}
