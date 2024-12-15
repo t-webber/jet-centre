0c4cd8d2d7f5b855ac7d612f02ce228bca4a4855
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-15 18:53:51 +0100

    (navbar) Add nbsp to title
    - From 'JET CENTER' to 'JET&nbsp;CENTER'

--- src/components/navigation/navbar-header.tsx
+++ src/components/navigation/navbar-header.tsx
@@ -5,7 +5,7 @@ export const NavbarHeader = () => {
         <a href="/" className="flex items-center gap-3 px-2">
             <LogoBird />
             <div className="flex flex-col">
-                <h1 className="font-semibold text-lg">JET CENTER</h1>
+                <h1 className="font-semibold text-lg">JET&nbsp;CENTER</h1>
                 <p className="text-sm text-muted-foreground">Telecom Etude</p>
             </div>
         </a>
