f9ca13ca22700914144f1f7a868b7165ef6a613a
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-15 18:54:19 +0100

    (combobox) Improve ui

--- src/components/meta-components/combobox.tsx
+++ src/components/meta-components/combobox.tsx
@@ -112,7 +112,7 @@ export const SingleCombobox = ({
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
+            <PopoverContent className="p-0">
                 <Command>
                     <CommandInput placeholder={placeholder} />
                     <CommandList>
