34ec247d03dc935ea2f47b03e2fd45f79d29e035
Tatounee <66035819+Tatounee@users.noreply.github.com>
2024-12-29 19:36:08 +0100

    (checkbox) Define two style for checkbox
    - Normal one (white and gray) and primary one (orange and check-mark)

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -6,27 +6,39 @@ import { Check } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
-const Checkbox = React.forwardRef<
-    React.ElementRef<typeof CheckboxPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-    <CheckboxPrimitive.Root
-        ref={ref}
-        className={cn(
-            'h-4 w-4 shrink-0 rounded-sm border-[3px] border-checkbox-border ring-offset-background',
-            'data-[state=checked]:bg-checkbox-background data-[state=checked]:text-primary-foreground',
-            'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-            'disabled:cursor-not-allowed disabled:opacity-50',
-            className
-        )}
-        {...props}
-    >
-        <CheckboxPrimitive.Indicator className={cn('h-full w-full bg-slate-200')}>
-            {/* <Check className="h-4 w-4" /> */}
-            {/* <div className="h-4 w-4 bg-slate-200"></div> */}
-        </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-));
+interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+    primary?: boolean;
+}
+
+const Checkbox = React.forwardRef<React.ElementRef<typeof CheckboxPrimitive.Root>, CheckboxProps>(
+    ({ className, primary = false, ...props }, ref) => (
+        <CheckboxPrimitive.Root
+            ref={ref}
+            className={cn(
+                'h-4 w-4 shrink-0 rounded-sm ring-offset-background',
+                'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+                'disabled:cursor-not-allowed disabled:opacity-50',
+
+                primary
+                    ? 'peer border border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground'
+                    : 'border-[2.5px] border-checkbox-border data-[state=checked]:bg-checkbox-background data-[state=checked]:text-primary-foreground',
+
+                className
+            )}
+            {...props}
+        >
+            <CheckboxPrimitive.Indicator
+                className={cn(
+                    primary
+                        ? 'flex items-center justify-center text-current -translate-y-[1px]'
+                        : 'h-full w-full bg-slate-200'
+                )}
+            >
+                {primary && <Check className="h-4 w-4" />}
+            </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+    )
+);
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export { Checkbox };
