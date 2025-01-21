41a576694b6953e43bd7c0c84772a9d575b01e11
Tom Webber <t.c.w.webber@gmail.com>
2025-01-21 09:12:04 +0100

    (doc) add: root files documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,59 @@
 import './globals.css';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { cn } from '@/lib/utils';
 import localFont from 'next/font/local';
+import { NextFont } from 'next/dist/compiled/@next/font';
 
-const avenir = localFont({
+/**
+ * Font used by the website, this is the font that is part of our corporate identity. This font mus't be modified.
+ *
+ * @type {NextFont}
+ */
+const avenir: NextFont = localFont({
     src: [
         {
             path: '../fonts/Avenir-Bold.otf',
             weight: '700',
-            style: 'normal',
+            style: 'normal'
         },
         {
             path: '../fonts/Avenir-Demi.otf',
             weight: '600',
-            style: 'normal',
+            style: 'normal'
         },
         {
             path: '../fonts/Avenir-DemiIt.otf',
             weight: '600',
-            style: 'italic',
+            style: 'italic'
         },
         {
             path: '../fonts/Avenir-It.otf',
             weight: '400',
-            style: 'italic',
+            style: 'italic'
         },
         {
             path: '../fonts/Avenir-Regular.otf',
             weight: '400',
-            style: 'normal',
-        },
-    ],
+            style: 'normal'
+        }
+    ]
 });
 
+/**
+ * Root layout: everything of the website is rendered inside this.
+ *
+ * @default
+ * @export
+ * @async
+ * @param {children: ReactNode} - children is the the component of the rendered
+ * `page.tsx` corresponding to the url of the client
+ * @return {Promise<ReactNode>}
+ */
 export default async function RootLayout({
-    children,
+    children
 }: Readonly<{
-    children: React.ReactNode;
-}>) {
+    children: ReactNode;
+}>): Promise<ReactNode> {
     return (
         <html lang="fr">
             <body className={cn(avenir.className, 'dark h-dvh')}>{children}</body>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,9 @@
+/**
+ * Page rendered when no page was found. This is a default 404 page.
+ *
+ * @file not-found.tsx
+ */
+
 export default () => {
     return (
         <div>
