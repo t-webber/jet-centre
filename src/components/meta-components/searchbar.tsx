4cb04e80f3b3326d45abdad7b22139ea39cdb6eb
Tatounee <66035819+Tatounee@users.noreply.github.com>
2025-01-24 02:00:07 +0100

    (SearchBar) Increase label height

--- src/components/meta-components/searchbar.tsx
+++ src/components/meta-components/searchbar.tsx
@@ -61,9 +61,9 @@ export function SearchBar<T>({
             <div className={cn('group relative flex flex-row', className)}>
                 <Label
                     className={cn(
-                        'absolute left-2 top-4 bg-box-background w-fit max-w-[calc(100%-0.75rem)] px-1 text-lg line-h leading-4 rounded-md whitespace-nowrap pointer-events-none transition-all overflow-ellipsis overflow-hidden z-10',
-                        (search ?? '').toString() !== '' && 'left-2 -top-2',
-                        'group-focus-within:left-2 group-focus-within:-top-2 group-focus-within:max-w-none'
+                        'absolute left-2 top-3 bg-box-background w-fit max-w-[calc(100%-0.75rem)] px-1 py-1 text-lg line-h leading-4 rounded-md whitespace-nowrap pointer-events-none transition-all overflow-ellipsis overflow-hidden z-10',
+                        (search ?? '').toString() !== '' && 'left-2 -top-2 py-0',
+                        'group-focus-within:left-2 group-focus-within:-top-2 group-focus-within:max-w-none group-focus-within:py-0'
                     )}
                     htmlFor={id}
                 >
@@ -104,10 +104,8 @@ export function SearchBar<T>({
                                     key={index}
                                     onSelect={() => {
                                         setInFocus(false);
-                                        // setSearch(displayValue(item));
                                         setSearch('');
                                         onSelect_ && onSelect_(item);
-                                        // onSelect_?.(item);
                                     }}
                                     value={displayValue(item)}
                                 >
