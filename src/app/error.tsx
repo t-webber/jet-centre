f6ff70eb88e715a024d0b381c89d48cf66afcbe0
Tom Webber <t.c.w.webber@gmail.com>
2025-03-30 14:21:23 +0200

    (routing) add: error page with sidebar and topbar

---
+++ src/app/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+/**
+ * Page rendered when a thrown error wasn't caught.
+ *
+ * @file error.tsx
+ *
+ */
+
+import { LogoBird } from '@/components/logo/logo';
+
+export default function Error() {
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center gap-main">
+            <LogoBird />
+            <h1 className="text-3xl">500 - Internal error</h1>
+            <p>Merci de reporter ce bug au pôle info.</p>
+        </div>
+    );
+}
