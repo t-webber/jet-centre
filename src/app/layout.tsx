060879488c5769eee7824c1bf1a1148cd430269e
Tom Webber <t.c.w.webber@gmail.com>
2025-03-30 14:20:33 +0200

    (sidebar) fix: keeps sidebar and top bar on 404 page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import React, { ReactNode } from 'react';
 import { cn } from '@/lib/utils';
 import localFont from 'next/font/local';
 import { NextFont } from 'next/dist/compiled/@next/font';
+import { TopBar } from '@/components/navigation/topbar';
+import { SidebarApp } from '@/components/navigation/sidebar/sidebar-app';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { SessionProvider } from 'next-auth/react';
+import { get_user_sidebar_info } from '@/actions/cdp';
+import { auth } from '@/actions/auth';
+import { Session } from 'next-auth';
 
 /**
  * Font used by the website, this is the font that is part of our corporate identity. This font mus't be modified.
@@ -54,9 +61,33 @@ export default async function RootLayout({
 }: Readonly<{
     children: ReactNode;
 }>): Promise<ReactNode> {
+    const session = await auth();
+
     return (
         <html lang="fr">
-            <body className={cn(avenir.className, 'dark h-dvh')}>{children}</body>
+            <body className={cn(avenir.className, 'dark h-dvh')}>
+                {session ? <AdminSideBar session={session}>{children}</AdminSideBar> : children}
+            </body>
         </html>
+    );
+}
+
+async function AdminSideBar({ children, session }: { children: ReactNode; session: Session }) {
+    return (
+        <SessionProvider>
+            <SidebarProvider>
+                <SidebarApp
+                    userInfo={await get_user_sidebar_info(session.user.email || undefined)}
+                />
+                <SidebarInset>
+                    <header className="flex px-4 p-2 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+                        <TopBar />
+                    </header>
+                    <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                        <div className="min-h-0 flex-1">{children}</div>
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </SessionProvider>
     );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,10 +4,13 @@
  * @file not-found.tsx
  */
 
+import { LogoBird } from '@/components/logo/logo';
+
 export default function NotFound() {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
+        <div className="h-full w-full flex flex-col items-center justify-center gap-main">
+            <LogoBird />
+            <h1 className="text-3xl">404 - Page Not Found</h1>
         </div>
     );
 }
