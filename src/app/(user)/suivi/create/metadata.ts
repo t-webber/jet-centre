0e8af77ac7542044ee6acfe1a50d147d930c1fcd
Tom Webber <t.c.w.webber@gmail.com>
2025-05-14 20:39:31 +0200

    (main) fix: remove unused files

--- src/app/(user)/suivi/create/metadata.ts
+++
@@ -1,5 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata = {
-    title: "Création d'une étude",
-} satisfies Metadata;
--- src/components/docs/monaco.tsx
+++
@@ -1,3 +0,0 @@
-export default function Monaco() {
-    return <div>helloo world</div>;
-}
