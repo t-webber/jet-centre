1eda701971e24f8ade785e96971b173ef5d64602
Tom Webber <t.c.w.webber@gmail.com>
2025-05-18 12:50:41 +0200

    ref(settings-page): better typing for update actions

--- src/app/(user)/cdp/[study]/settings/clients/action.ts
+++ src/app/(user)/cdp/[study]/settings/clients/action.ts
@@ -1,11 +1,17 @@
 'use server';
 
-import { Address, Company, CompanyInfos } from '@prisma/client';
+import { Address, Prisma } from '@prisma/client';
 
 import prisma from '@/db';
 import { dbg } from '@/lib/utils';
 
-import { AddressType, ClientFormType, CompanyType, StudyClientsFormType } from './schema';
+import { ClientFormType, StudyClientsFormType } from './schema';
+
+type Data<UpdateInput, UncheckedUpdateInput> =
+    | (Prisma.Without<UpdateInput, UncheckedUpdateInput> & UncheckedUpdateInput)
+    | (Prisma.Without<UncheckedUpdateInput, UpdateInput> & Prisma.ClientUpdateInput);
+
+type PlainAddress = Omit<Address, 'id' | 'personId' | 'companyId'>;
 
 export async function getMissionClients(
     studyCode: string
@@ -43,51 +49,52 @@ export async function getMissionClients(
         if (!study) {
             throw new Error('Erroneous study code');
         }
+
+        const clients: ClientFormType[] = study.clients.map(({ id, client }) => {
+            const missionClient: ClientFormType = {
+                studyClientId: id,
+                clientId: client.id,
+                job: client.job,
+                firstName: client.person.firstName,
+                lastName: client.person.lastName,
+                email: client.person.email ?? undefined,
+                number: client.person.number ?? undefined,
+            };
+
+            if (client.person.address) {
+                missionClient.address = {
+                    number: client.person.address.streetNumber,
+                    street: client.person.address.streetName,
+                    city: client.person.address.city,
+                    country: client.person.address.country,
+                    zipCode: client.person.address.zipCode,
+                };
+            }
+
+            if (client.company) {
+                missionClient.company = {
+                    name: client.company.name,
+                    size: client.company.companyInfos.size ?? undefined,
+                    ca: client.company.companyInfos.ca ?? undefined,
+                    domains: client.company.companyInfos.domains,
+                    id: client.company.companyInfos.id,
+                };
+
+                if (client.company.address) {
+                    missionClient.company.address = {
+                        number: client.company.address.streetNumber,
+                        street: client.company.address.streetName,
+                        city: client.company.address.city,
+                        country: client.company.address.country,
+                        zipCode: client.company.address.zipCode,
+                    };
+                }
+            }
+            return missionClient;
+        });
+
         return {
-            clients: study.clients.map(({ id, client }) => {
-                let clientAddress: AddressType | undefined;
-                if (client.person.address) {
-                    clientAddress = {
-                        number: client.person.address.streetNumber,
-                        street: client.person.address.streetName,
-                        city: client.person.address.city,
-                        country: client.person.address.country,
-                        zipCode: client.person.address.zipCode,
-                    };
-                }
-                let company: CompanyType | undefined;
-                if (client.company) {
-                    let companyAddress: AddressType | undefined;
-                    if (client.company.address) {
-                        companyAddress = {
-                            number: client.company.address.streetNumber,
-                            street: client.company.address.streetName,
-                            city: client.company.address.city,
-                            country: client.company.address.country,
-                            zipCode: client.company.address.zipCode,
-                        };
-                    }
-                    company = {
-                        address: companyAddress,
-                        name: client.company.name,
-                        size: client.company.companyInfos.size ?? undefined,
-                        ca: client.company.companyInfos.ca ?? undefined,
-                        domains: client.company.companyInfos.domains,
-                        id: client.company.companyInfos.id,
-                    };
-                }
-                return {
-                    studyClientId: id,
-                    clientId: client.id,
-                    job: client.job,
-                    firstName: client.person.firstName,
-                    lastName: client.person.lastName,
-                    email: client.person.email ?? undefined,
-                    address: clientAddress,
-                    number: client.person.number ?? undefined,
-                    company,
-                } satisfies ClientFormType;
-            }),
+            clients,
             studyId: study.id,
         };
     } catch (e) {
@@ -95,139 +102,40 @@ export async function getMissionClients(
     }
 }
 
-type IdlessAddressInfos = Omit<Address, 'id'>;
-type IdlessCompanyInfos = Omit<CompanyInfos, 'id'>;
-type IdlessAllCompany = Omit<Company, 'id' | 'companyInfosId' | 'addressId'>;
-
-type IdlessAddressUpsert =
-    | { update: IdlessAddressInfos }
-    | { create: IdlessAddressInfos }
-    | undefined;
-
-export async function updateClient(clientId: string, clientData: ClientFormType) {
-    try {
-        let clientAddress: { update: Omit<Address, 'id'> } | undefined;
-
-        if (clientData.address) {
-            clientAddress = {
-                update: {
-                    streetNumber: clientData.address?.number,
-                    streetName: clientData.address?.street,
-                    city: clientData.address?.city,
-                    country: clientData.address?.country,
-                    zipCode: clientData.address?.zipCode,
-                },
-            };
-        }
-
-        let companyAddress: IdlessAddressUpsert;
-
-        if (clientData.company?.address) {
-            if (clientData.company.address.id) {
-                companyAddress = {
-                    update: {
-                        streetNumber: clientData.company.address?.number,
-                        streetName: clientData.company.address?.street,
-                        city: clientData.company.address?.city,
-                        country: clientData.company.address?.country,
-                        zipCode: clientData.company.address?.zipCode,
-                    },
-                };
-            } else
-                companyAddress = {
-                    create: {
-                        streetNumber: clientData.company.address?.number,
-                        streetName: clientData.company.address?.street,
-                        city: clientData.company.address?.city,
-                        country: clientData.company.address?.country,
-                        zipCode: clientData.company.address?.zipCode,
-                    },
-                };
-        }
-
-        let company:
-            | {
-                  update: {
-                      where: { id: string };
-                      data: IdlessAllCompany & {
-                          companyInfos: { update: IdlessCompanyInfos };
-                          address: IdlessAddressUpsert;
-                      };
-                  };
-              }
-            | {
-                  create: IdlessAllCompany & {
-                      companyInfos: { create: IdlessCompanyInfos };
-                      address: { create: IdlessAddressInfos };
-                  };
-              }
-            | undefined;
-
-        if (clientData.company?.id) {
-            company = {
-                update: {
-                    where: { id: clientData.company.id },
-                    data: {
-                        name: clientData.company.name,
-                        companyInfos: {
-                            update: {
-                                size: clientData.company.size ?? null,
-                                ca: clientData.company.ca ?? null,
-                                domains: clientData.company.domains,
-                            },
-                        },
-                        address: companyAddress,
-                    },
-                },
-            };
-        } else if (clientData.company) {
-            if (clientData.address?.id !== undefined)
-                throw new Error(
-                    "Address id defined but company id undefined: can't updated address for non-created company."
-                );
-
-            company = {
-                create: {
-                    name: clientData.company.name,
-                    companyInfos: {
-                        create: {
-                            size: clientData.company.size ?? null,
-                            ca: clientData.company.ca ?? null,
-                            domains: clientData.company.domains,
-                        },
-                    },
-                    address: companyAddress as { create: IdlessAddressInfos },
-                },
-            };
-        }
-
-        await prisma.client.update({
-            where: { id: clientId },
-            data: {
-                job: clientData.job,
-                person: {
-                    update: {
-                        firstName: clientData.firstName,
-                        lastName: clientData.lastName,
-                        email: clientData.email,
-                        number: clientData.number,
-                        address: clientAddress,
-                    },
-                },
-                company,
-            },
-        });
-    } catch (e) {
-        console.error(`[updateClient] ${e}`);
-    }
-}
-
 export async function addClient(studyId: string, clientData: ClientFormType) {
     try {
         dbg(clientData, `adding data for ${studyId}`);
-        let address: { create: Omit<Address, 'id'> } | undefined;
+
+        const data: Data<Prisma.StudyUpdateInput, Prisma.StudyUncheckedUpdateInput> = {
+            clients: {
+                create: {
+                    client: {
+                        create: {
+                            job: clientData.job,
+                            person: {
+                                create: {
+                                    firstName: clientData.firstName,
+                                    lastName: clientData.lastName,
+                                    email: clientData.email,
+                                    number: clientData.number,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        };
+
+        if (
+            !data.clients?.create ||
+            !('client' in data.clients.create) ||
+            !data.clients?.create?.client?.create?.person?.create
+        ) {
+            throw new Error('unreachable: missing create on person');
+        }
+
         if (clientData.address) {
-            address = {
+            data.clients.create.client.create.person.create.address = {
                 create: {
                     streetNumber: clientData.address?.number,
                     streetName: clientData.address?.street,
@@ -237,22 +145,12 @@ export async function addClient(studyId: string, clientData: ClientFormType) {
                 },
             };
         }
-        let connectCompany: { connect: { id: string } } | undefined;
+
         if (clientData.company) {
-            dbg(clientData.company, 'haha company');
-            const company = await prisma.company.update({
-                where: { id: clientData.company.id },
-                data: {
+            data.clients.create.client.create.company = {
+                create: {
                     name: clientData.company?.name,
-                    address: {
-                        update: {
-                            streetNumber: clientData.company?.address?.number,
-                            streetName: clientData.company?.address?.street,
-                            city: clientData.company?.address?.city,
-                            country: clientData.company?.address?.country,
-                            zipCode: clientData.company?.address?.zipCode,
-                        },
-                    },
+                    address: {},
                     companyInfos: {
                         create: {
                             domains: clientData.company?.domains,
@@ -261,39 +159,131 @@ export async function addClient(studyId: string, clientData: ClientFormType) {
                         },
                     },
                 },
-            });
-            connectCompany = {
-                connect: {
-                    id: company.id,
-                },
             };
+
+            if (!data.clients.create.client.create.company.create)
+                throw new Error('unreachable: missing create on company');
+            if (clientData.company.address) {
+                data.clients.create.client.create.company.create.address = {
+                    create: {
+                        streetNumber: clientData.company?.address?.number,
+                        streetName: clientData.company?.address?.street,
+                        city: clientData.company?.address?.city,
+                        country: clientData.company?.address?.country,
+                        zipCode: clientData.company?.address?.zipCode,
+                    },
+                };
+            }
         }
+
         await prisma.study.update({
             where: { id: studyId },
-            data: {
-                clients: {
-                    create: {
-                        client: {
-                            create: {
-                                job: clientData.job,
-                                person: {
-                                    create: {
-                                        firstName: clientData.firstName,
-                                        lastName: clientData.lastName,
-                                        email: clientData.email,
-                                        number: clientData.number,
-                                        address,
-                                    },
-                                },
-                                company: connectCompany,
-                            },
-                        },
-                    },
-                },
-            },
+            data,
         });
     } catch (e) {
         console.error(`[addClient] ${e}`);
+    }
+}
+
+export async function updateClient(clientId: string, clientData: ClientFormType) {
+    try {
+        dbg(clientData, '############## new client data ##############');
+
+        const data: Data<Prisma.ClientUpdateInput, Prisma.ClientUncheckedUpdateInput> = {
+            job: clientData.job,
+            person: {
+                update: {
+                    firstName: clientData.firstName,
+                    lastName: clientData.lastName,
+                    email: clientData.email,
+                    number: clientData.number,
+                },
+            },
+        };
+
+        if (!data.person?.update) throw new Error('unreachable: missing update on person');
+
+        if (clientData.address) {
+            const address: PlainAddress = {
+                streetNumber: clientData.address.number,
+                streetName: clientData.address.street,
+                city: clientData.address.city,
+                country: clientData.address.country,
+                zipCode: clientData.address.zipCode,
+            };
+
+            if (clientData.address.id) {
+                data.person.update.address = {
+                    update: address,
+                };
+            } else {
+                data.person.update.address = {
+                    create: address,
+                };
+            }
+        }
+
+        if (clientData.company) {
+            if (clientData.company.id) {
+                data.company = {
+                    update: {
+                        where: { id: clientData.company.id },
+                        data: {
+                            name: clientData.company.name,
+                            companyInfos: {
+                                update: {
+                                    size: clientData.company.size ?? null,
+                                    ca: clientData.company.ca ?? null,
+                                    domains: clientData.company.domains,
+                                },
+                            },
+                        },
+                    },
+                };
+            } else {
+                data.company = {
+                    create: {
+                        name: clientData.company.name,
+                        companyInfos: {
+                            create: {
+                                size: clientData.company.size ?? null,
+                                ca: clientData.company.ca ?? null,
+                                domains: clientData.company.domains,
+                            },
+                        },
+                    },
+                };
+            }
+
+            if (clientData.company.address) {
+                const address: PlainAddress = {
+                    streetNumber: clientData.company.address?.number,
+                    streetName: clientData.company.address?.street,
+                    city: clientData.company.address?.city,
+                    country: clientData.company.address?.country,
+                    zipCode: clientData.company.address?.zipCode,
+                };
+
+                if (data.company.create && clientData.address?.id) {
+                    throw new Error("unreachable: address can't exist without company");
+                } else if (data.company.create) {
+                    data.company.create.address = { create: address };
+                } else if (data.company.update && clientData.address?.id) {
+                    data.company.update.address = { update: address };
+                } else if (data.company.update) {
+                    data.company.update.address = { create: address };
+                } else throw new Error('unreachable: company has neither create or update');
+            }
+        }
+
+        dbg(data, '############## data ##############');
+
+        await prisma.client.update({
+            where: { id: clientId },
+            data,
+        });
+    } catch (e) {
+        console.error(`[updateClient] ${e}`);
     }
 }
 
