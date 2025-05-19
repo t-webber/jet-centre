df795be1c8622af72e56ecd65a9fe5d391d8096a
Tom Webber <t.c.w.webber@gmail.com>
2025-05-19 23:48:45 +0200

    fix(knip): remove unused exports and objects

--- src/app/(user)/cdp/[study]/settings/clients/schema.ts
+++ src/app/(user)/cdp/[study]/settings/clients/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 import { arrayEqual } from '@/lib/utils';
 
-export const addressSchema = z.object({
+const addressSchema = z.object({
     id: z.string().optional(),
     number: z.string(),
     street: z.string(),
@@ -12,9 +12,7 @@ export const addressSchema = z.object({
     zipCode: z.string(),
 });
 
-export type AddressType = z.infer<typeof addressSchema>;
-
-export const companySchema = z.object({
+const companySchema = z.object({
     id: z.string().optional(),
     name: z.string(),
     address: addressSchema.optional(),
@@ -22,8 +20,6 @@ export const companySchema = z.object({
     ca: z.number().optional(),
     domains: z.array(z.nativeEnum(Domain)),
 });
-
-export type CompanyType = z.infer<typeof companySchema>;
 
 export const clientFormSchema = z.object({
     studyClientId: z.string().optional(),
@@ -37,34 +33,20 @@ export const clientFormSchema = z.object({
     address: addressSchema.optional(),
 });
 
-export type ClientFormType = z.infer<typeof clientFormSchema>;
+type clientFormSchemaTypes = z.ZodObject<{
+    studyClientId: z.ZodOptional<z.ZodString>;
+    clientId: z.ZodOptional<z.ZodString>;
+    company: z.ZodOptional<typeof companySchema>;
+    job: z.ZodString;
+    firstName: z.ZodString;
+    lastName: z.ZodString;
+    number: z.ZodOptional<z.ZodString>;
+    email: z.ZodOptional<z.ZodString>;
+    address: z.ZodOptional<typeof addressSchema>;
+}>;
 
-export const studyClientsFormSchema = z.object({
-    clients: z.array(clientFormSchema),
-    studyId: z.string(),
-});
+export type ClientFormType = z.infer<clientFormSchemaTypes>;
 
-export type StudyClientsFormType = z.infer<typeof studyClientsFormSchema>;
-
-export function checkEqual(lhs: StudyClientsFormType, rhs: StudyClientsFormType) {
-    return (
-        lhs.studyId == rhs.studyId &&
-        arrayEqual(
-            lhs.clients,
-            rhs.clients,
-            (client1, client2) =>
-                client1.clientId === client2.clientId &&
-                client1.studyClientId === client2.studyClientId &&
-                client1.job === client2.job &&
-                client1.firstName === client2.firstName &&
-                client1.lastName === client2.lastName &&
-                client1.number === client2.number &&
-                client1.email === client2.email &&
-                client1.address?.number === client2.address?.number &&
-                client1.address?.street === client2.address?.street &&
-                client1.address?.city === client2.address?.city &&
-                client1.address?.country === client2.address?.country &&
-                client1.address?.zipCode === client2.address?.zipCode
-        )
-    );
-}
+export type StudyClientsFormType = z.infer<
+    z.ZodObject<{ clients: z.ZodArray<clientFormSchemaTypes>; studyId: z.ZodString }>
+>;
