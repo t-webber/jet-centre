6d20c3ff476f35924a569a7d4179af47848ec1f3
Tom Webber <t.c.w.webber@gmail.com>
2025-07-19 11:09:50 +0200

    fix(list-companies): only load companies and not individual clients

--- src/app/(user)/suivi/list-companies/actions.ts
+++ src/app/(user)/suivi/list-companies/actions.ts
@@ -1,28 +1,12 @@
 'use server';
 
 import db from '@/db';
+import { CompanyName } from './types';
 
-export interface ClientListItem {
-    id: string;
-    name: string;
-    independant?: boolean;
-}
-
-export async function getClients(): Promise<ClientListItem[] | undefined> {
+export async function getCompanies(): Promise<CompanyName[] | undefined> {
     try {
         const companies = await db.company.findMany({ select: { id: true, name: true } });
-        const raw_independant_clients = await db.client.findMany({
-            where: { company: undefined },
-            select: { id: true, person: { select: { firstName: true, lastName: true } } },
-        });
-        const independant_clients = raw_independant_clients.map(
-            ({ id, person: { firstName, lastName } }) => ({
-                id,
-                name: firstName + ' ' + lastName,
-                independant: true,
-            })
-        );
-        return companies.concat(independant_clients);
+        return companies;
     } catch (e) {
         console.error(`[getCompanies] ${e}`);
     }
