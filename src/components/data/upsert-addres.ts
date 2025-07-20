c70e21f573e0c16fd76b2859428ae8576069ab43
Tom Webber <t.c.w.webber@gmail.com>
2025-07-20 13:14:50 +0200

    fix(components): upsert needs a non-null id

--- src/components/data/upsert-addres.ts
+++ src/components/data/upsert-addres.ts
@@ -6,14 +6,29 @@ import { Address } from '@prisma/client';
 export async function upsertAddress(
     address: Omit<Address, 'id'> & { id: string | undefined }
 ): Promise<Address | undefined> {
-    try {
-        const dbAddress = await db.address.upsert({
-            where: { id: address.id },
-            update: address,
-            create: address,
-        });
-        return dbAddress;
-    } catch (e) {
-        console.error(`[upsertAddress] ${e}`);
+    if (address.id === undefined) {
+        try {
+            const dbAddress = await db.address.create({
+                data: address,
+            });
+            return dbAddress;
+        } catch (e) {
+            console.error(
+                `[upsertAddress-create] ${e}\n----------\n\x1b[31m${JSON.stringify(address)}\x1b[0m\n----------\n`
+            );
+        }
+    } else {
+        try {
+            const dbAddress = await db.address.upsert({
+                where: { id: address.id },
+                update: address,
+                create: address,
+            });
+            return dbAddress;
+        } catch (e) {
+            console.error(
+                `[upsertAddress-upsert] ${e}\n----------\n\x1b[31m${JSON.stringify(address)}\x1b[0m\n----------\n`
+            );
+        }
     }
 }
