30435d8ef0be258d49e7a120279e4827149ca164
Tom Webber <t.c.w.webber@gmail.com>
2025-07-20 21:45:16 +0200

    fix(loading): don't display loading page if the loading time is short

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -8,7 +8,18 @@
  */
 
 import { ErrorPage } from '@/components/error';
+import { useEffect, useState } from 'react';
+
+const DELAY_TO_SHOW_LOADING_PAGE = 500; // milliseconds
 
 export default function Loading() {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setShow(true), DELAY_TO_SHOW_LOADING_PAGE);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (!show) return null;
     return <ErrorPage title="Loading..." />;
 }
