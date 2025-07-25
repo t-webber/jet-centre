e8acc534ff1f05d6bd34f1be9bdb0f4b2593c3d9
Tom Webber <t.c.w.webber@gmail.com>
2025-07-25 19:02:27 +0200

    fix(error-page): center text and add padding for small displays

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -9,9 +9,9 @@ interface ErrorPageProps {
 
 export function ErrorPage({ title, children }: ErrorPageProps) {
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center gap-main">
+        <div className="p-main h-full w-full flex flex-col items-center justify-center gap-main">
             <LogoBird />
-            <h1 className="text-3xl">{title}</h1>
+            <h1 className="text-3xl text-center">{title}</h1>
             {children}
         </div>
     );
