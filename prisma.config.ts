1ca7a9d0ef9bae067ff5787cda1ab5a7cc0df1a5
Rémy Fayet <rems64des@gmail.com>
2025-08-01 19:38:18 +0200

    chore(docker): add early access back to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 import path from 'node:path';
+
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
+    earlyAccess: true,
     schema: path.join('prisma'),
 });
