9ee24c460865726a9b2593efb41da6353f7ceaac
Tom Webber <t.c.w.webber@gmail.com>
2025-08-01 19:46:33 +0200

    docs(env): explain meaning of each environment variable in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,34 +1,101 @@
+/**
+ * Environment variables needed by auth.js
+ */
+interface AuthEnv {
+    /**
+     * Secret used for signing authentication tokens.
+     * @see https://authjs.dev/getting-started/security#secret
+     */
+    AUTH_SECRET: string;
+    /**
+     * Google OAuth client ID.
+     * @see https://console.cloud.google.com/apis/credentials
+     */
+    AUTH_GOOGLE_ID: string;
+    /**
+     * Google OAuth client secret.
+     * @see https://console.cloud.google.com/apis/credentials
+     */
+    AUTH_GOOGLE_SECRET: string;
+    /**
+     * Needed if you want to use a production build on localhost.
+     * Contains the URL, e.g. `http://localhost:5005`.
+     * @see https://authjs.dev/reference/core/types#trusthost
+     */
+    AUTH_TRUST_HOST?: string;
+}
+
+/**
+ * Environment variables needed for Google Drive integration
+ */
+interface GoogleDriveEnv {
+    /**
+     * Google Drive folder ID for the "Dossier de suivi".
+     */
+    DOSSIER_SUIVI: string;
+    /**
+     * All the following are the Google ids to the templates
+     * */
+    TEMPLATE_EXCEL_CE: string;
+    TEMPLATE_EXCEL_RM: string;
+    TEMPLATE_ACC: string;
+    TEMPLATE_ACE: string;
+    TEMPLATE_ARM: string;
+    TEMPLATE_BC: string;
+    TEMPLATE_BCR: string;
+    TEMPLATE_BA: string;
+    TEMPLATE_CC: string;
+    TEMPLATE_CE: string;
+    TEMPLATE_PVRF: string;
+    TEMPLATE_PVRI: string;
+    TEMPLATE_RM: string;
+}
+
+/**
+ * Environment variables needed for Google Drive integration
+ */
+interface SeedEnv {
+    /**
+     * Email address of the initial admin user.
+     * */
+    ADMIN_EMAIL: string;
+    /**
+     * Position of the initial admin user. Defaults to 'Info'. The position determines the rights you will have access to when first signing in
+     */
+    ADMIN_POSITION?: string;
+    /**
+     * Deployment environment: "prod" or "dev". This influences how the seed is performed.
+     */
+    ENV: 'prod' | 'dev';
+    /**
+     * If true, disables DB initialisation logic on seed.
+     * Otherwise, the DB is filled with lots of data.
+     * */
+    NO_DB_INIT?: true;
+}
+
+interface OtherEnv {
+    WEBSITE_URL: string;
+    /**
+     * @see ./docs/database.md
+     */
+    DB_URL: string;
+    /**
+     * Disables REDIS
+     * */
+    NO_CACHE?: string;
+    /**
+     * Redis connection URI.
+     * @see https://redis.io/docs/latest/operate/configuration/cli/connection-strings/
+     */
+    REDIS_URL: string;
+    /**
+     * Gives unconditional access to all data and pages.
+     * This breaks middleware logic.
+     */
+    DEV_MODE?: true;
+}
+
 declare namespace NodeJS {
-    interface ProcessEnv {
-        /// Auth
-        AUTH_SECRET: string;
-        AUTH_GOOGLE_ID: string;
-        AUTH_GOOGLE_SECRET: string;
-        AUTH_TRUST_HOST?: string;
-        /// Google Ids
-        DOSSIER_SUIVI: string;
-        TEMPLATE_EXCEL_CE: string;
-        TEMPLATE_EXCEL_RM: string;
-        TEMPLATE_ACC: string;
-        TEMPLATE_ACE: string;
-        TEMPLATE_ARM: string;
-        TEMPLATE_BC: string;
-        TEMPLATE_BCR: string;
-        TEMPLATE_BA: string;
-        TEMPLATE_CC: string;
-        TEMPLATE_CE: string;
-        TEMPLATE_PVRF: string;
-        TEMPLATE_PVRI: string;
-        TEMPLATE_RM: string;
-        /// Seed
-        ADMIN_EMAIL: string;
-        ADMIN_POSITION?: string;
-        ENV: 'prod' | 'dev';
-        /// Others
-        WEBSITE_URL: string;
-        DB_URL: string;
-        NO_CACHE?: string;
-        REDIS_URL: string;
-        DEV_MODE?: true;
-    }
+    interface ProcessEnv extends AuthEnv, GoogleDriveEnv, SeedEnv, OtherEnv {}
 }
