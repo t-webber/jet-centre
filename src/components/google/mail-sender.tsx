6a09134eeb1fa66e94063fcce8527bbe9e0796e5
Tom Webber <t.c.w.webber@gmail.com>
2025-08-14 23:48:02 +0200

    feat(gmail): add functionalities for email sender
    Added support for
    - Html/plain text
    - cc/bcc
    - reply-to

--- src/components/google/mail-sender.tsx
+++ src/components/google/mail-sender.tsx
@@ -2,20 +2,38 @@
 
 import { useState } from 'react';
 
-import { sendHelloWorld } from '@/google/mail/send';
+import { sendEmail } from '@/google/mail/send';
 
 import { Input } from '../ui/input';
 
 export function MailSender() {
     const [value, setValue] = useState('');
+    const [good, setGood] = useState<boolean | undefined>();
 
-    const sendEmail = () => {
-        if (value) sendHelloWorld(value);
+    const onBlur = () => {
+        if (value) {
+            setGood(undefined);
+            sendEmail({
+                to: value,
+                subject: 'Hello, World!',
+                html: '<p style="color: red">Hello world</p>"',
+                text: 'Hello world',
+                replyTo: 'info@telecom-etude.fr',
+            }).then((res) => setGood(res));
+        }
     };
 
     return (
         <Input
-            onBlur={() => sendEmail()}
+            onBlur={onBlur}
+            className={
+                'border-8 ' +
+                (good === true
+                    ? 'border-green-500'
+                    : good === false
+                      ? 'border-red-500'
+                      : 'border-blue-500')
+            }
             value={value}
             onChange={(e) => setValue(e.target.value)}
         />
--- src/google/mail/send.ts
+++ src/google/mail/send.ts
@@ -1,4 +1,3 @@
-'use server';
 /**
  * This file contains all the access to the Gmail API, with the send scope.
  *
@@ -6,33 +5,89 @@
  * as server functions in an way that is agnostic of how the API actually works.
  */
 
+'use server';
+
 import { auth } from '@/actions/auth';
 import { dbg } from '@/lib/utils';
 
 import { googleMail } from '../api';
 
-export async function sendHelloWorld(to: string): Promise<boolean> {
-    const gmail = await googleMail();
-    const session = await auth();
-    const from = session?.user.email;
-
-    const body = `From: ${from}
-To: ${to}
-Subject: Hi
-
-Hello, world!`;
-    const encodedBody = Buffer.from(body)
+function base64UrlEncode(value: string): string {
+    return Buffer.from(value)
         .toString('base64')
         .replace(/\+/g, '-')
         .replace(/\//g, '_')
         .replace(/=+$/g, '');
+}
 
-    const sentEmail = await gmail.users.messages.send({
-        userId: 'me',
-        requestBody: { raw: encodedBody },
-    });
+export interface EmailBuilder {
+    from?: string;
+    to: string;
+    subject: string;
+    cc?: string[];
+    bcc?: string[];
+    replyTo?: string;
+    text?: string;
+    html?: string;
+}
 
-    dbg(sentEmail, 'sentEmail');
+function rfc2822FromEmailBuilder(emailBuilder: EmailBuilder): string {
+    let rfc2822Message = '';
 
-    return false;
+    rfc2822Message += `From: ${emailBuilder.from}\r\n`;
+    rfc2822Message += `To: ${emailBuilder.to}\r\n`;
+    rfc2822Message += `Subject: ${emailBuilder.subject}\r\n`;
+
+    if (emailBuilder.cc && emailBuilder.cc.length >= 1)
+        rfc2822Message += `Cc: ${emailBuilder.cc.join(', ')}\r\n`;
+    if (emailBuilder.bcc && emailBuilder.bcc.length >= 1)
+        rfc2822Message += `Bcc: ${emailBuilder.bcc.join(', ')}\r\n`;
+
+    if (emailBuilder.replyTo) rfc2822Message += `Reply-To: ${emailBuilder.replyTo}\r\n`;
+
+    rfc2822Message += `Date: ${new Date().toUTCString()}\r\n`;
+    rfc2822Message += `MIME-Version: 1.0\r\n`;
+
+    const boundary = '--------------------';
+
+    rfc2822Message += `Content-Type: multipart/alternative; boundary="${boundary}"\r\n\r\n`;
+
+    if (emailBuilder.text) {
+        rfc2822Message += `--${boundary}\r\n`;
+        rfc2822Message += `Content-Type: text/plain; charset=UTF-8\r\n\r\n`;
+        rfc2822Message += `${emailBuilder.text}\r\n\r\n`;
+    }
+
+    if (emailBuilder.html) {
+        rfc2822Message += `--${boundary}\r\n`;
+        rfc2822Message += `Content-Type: text/html; charset=UTF-8\r\n\r\n`;
+        rfc2822Message += `${emailBuilder.html}\r\n\r\n`;
+    }
+
+    rfc2822Message += `--${boundary}--`;
+
+    return dbg(rfc2822Message, 'msg');
+}
+
+export async function sendEmail(emailBuilder: EmailBuilder): Promise<boolean> {
+    try {
+        console.log('sending');
+        const gmail = await googleMail();
+        const session = await auth();
+
+        if (!emailBuilder.from) emailBuilder.from = session?.user.email ?? undefined;
+
+        const raw = base64UrlEncode(rfc2822FromEmailBuilder(emailBuilder));
+
+        await gmail.users.messages.send({
+            userId: 'me',
+            requestBody: { raw },
+        });
+
+        return true;
+    } catch (e) {
+        console.error(`[sendEmail] ${e}`);
+
+        return false;
+    }
 }
